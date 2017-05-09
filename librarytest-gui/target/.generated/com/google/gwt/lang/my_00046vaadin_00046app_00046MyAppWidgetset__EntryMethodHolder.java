35af270d2c6a9c99d63e365b795e6f800a14a7fb
Daniel Lundström <Daniel.Lundstrom@yh.nackademin.se>
2017-05-09 13:41:43 +0200

    added target directories

---
+++ librarytest-gui/target/.generated/com/google/gwt/lang/my_00046vaadin_00046app_00046MyAppWidgetset__EntryMethodHolder.java
@@ -0,0 +1,6 @@
+package com.google.gwt.lang;
+public class my_00046vaadin_00046app_00046MyAppWidgetset__EntryMethodHolder {
+  public static final void init() {
+    // to be synthesized later
+  }
+}
---
+++ librarytest-gui/target/classes/VAADIN/widgetsets/my.vaadin.app.MyAppWidgetset/deferredjs/F01DC37CF3932A3B8CDA2B0063E0477F/2.cache.js
@@ -0,0 +1,1 @@
+$wnd.my_vaadin_app_MyAppWidgetset.runAsyncCallback2('pdb(1601,1,g_d);_.vc=function Ogc(){S1b((!L1b&&(L1b=new X1b),L1b),this.a.d)};IUd(Th)(2);\n//# sourceURL=my.vaadin.app.MyAppWidgetset-2.js\n')
